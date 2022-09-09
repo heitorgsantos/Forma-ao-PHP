f64623b182fa5d76bdd6b0d596704d1a7908e16a
heitorgsantos <heitorg33@gmail.com>
2022-09-09 11:01:03 -0300

    Organizando arquivos

--- desafio1.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-for ($index = 1; $index <= 100; $index++) {
-    if ($index % 2 == 0) {
-        continue;
-    }
-    echo $index . PHP_EOL;
-}
--- desafio2.php
+++
@@ -1,5 +0,0 @@
-<?php
-$multiple = 1;
-for ($index = 1; $index <= 9; $index++) {
-    echo $finaly = $index * $multiple . PHP_EOL;
-}
